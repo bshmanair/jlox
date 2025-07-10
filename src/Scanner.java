05ff876a4c9618d0949b78c08b52026e81b11cc1
bshmanair <bhishu888@gmail.com>
2025-07-10 19:40:31 +0400

    Chapter 4 done

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,8 +1,9 @@
 // package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 class Scanner {
     private final String source;
@@ -10,6 +11,29 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static
+    {
+        keywords = new HashMap<>();
+
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
 
     Scanner(String source)
     {
@@ -95,11 +119,39 @@ class Scanner {
             default -> {
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
             }
         }
+    }
+    
+    private void identifier()
+    {
+        while (isAlphaNumeric(peek())) {
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+        {
+            type = TokenType.IDENTIFIER;
+        }
+
+        addToken(type);
+    }
+    
+    private boolean isAlpha(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c)
+    {
+        return isAlpha(c) || isDigit(c);
     }
     
     private boolean isDigit(char c)
@@ -125,12 +177,13 @@ class Scanner {
             advance();
         }
 
-        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+        addToken(TokenType.NUMBER, Double.valueOf(source.substring(start, current)));
     }
     
     private char peek()
     {
-        if (isAtEnd()) {
+        if (isAtEnd())
+        {
             return '\0';
         }
         return source.charAt(current);
